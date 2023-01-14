a461f45486cbf68f36d0846fe5d20a9548f14af4
Benson Njuguna <njugunab655@gmail.com>
2023-01-14 14:27:10 +0300

    Added a quotes component

---
+++ src/components/Quote.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Quote(props) {
+    return (
+        <div className="div">
+            <p className="quote"></p>
+            <h4 className="author"></h4>
+        </div>
+    )
+}
